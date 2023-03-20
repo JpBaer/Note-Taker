05db43a1e03d5ab8965c5d73f725cf45e9b834ab
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-03-20 15:11:08 -0700

    get and delete request written

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 // Upload express
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = 3001;
 const app = express();
 //Generates unique id tags for stored data
 const uuid = require('./helpers/uuid');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const readFromFile = util.promisify(fs.readFile);
 app.use(express.static('public'));
 // Shows the homepage anytime a url is entered aside from /notes
-app.get('*', (req, res) => {
-    res.sendFile(
-      `./public/index.html`)
-  });
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname,
+      `/public/index.html`))
+  );
   
 
 //Pulls notes.html when /notes is used at the end of url
-app.get('/notes',(req, res) => {
-    res.sendFile(`./public/notes.html`)
-})
 
-//Need to add code for a post request to add new entry into JSON file
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
 
 //Gets all data stored in JSON file and shows it on the page
-app.get('/api/notes',(req, res) => {
 
+app.get('/api/notes',(req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
 
@@ -44,7 +51,7 @@ app.post('/api/notes',(req, res) => {
     }
   }
 
-  fs.readFile(`./db/notes.json`,'utf8',(err,data)=>{
+  fs.readFile(`./db/db.json`,'utf8',(err,data)=>{
     if(err){
       console.error(err)
     }
@@ -53,7 +60,7 @@ app.post('/api/notes',(req, res) => {
     parsedNotes.push(newNote);
     const noteString = JSON.stringify(parsedNotes);
 
-    fs.writeFile(`./db/notes.json`, noteString, (err) =>
+    fs.writeFile(`./db/db.json`, noteString, (err) =>
     err
       ? console.error(err)
       : console.log(
@@ -69,8 +76,38 @@ app.post('/api/notes',(req, res) => {
 })
 
 //Deletes JSON data based on unique idea in url
-app.delete(`/api/notes/${id}`,(req, res) => {
+app.delete(`/api/notes/:id`,(req, res) => {
+  const id = req.params.id
+  console.log(id)
+   fs.readFile(`./db/db.json`,'utf8',(err,data)=>{
+    if(err){
+      console.error(err)
+    }
+    else{
 
+    const parsedNotes = JSON.parse(data);
+    console.log(parsedNotes)
+  
+   const noteIndex = parsedNotes.findIndex(n => n.id == id)
+  //Loop through parsed Notes to find the note with the same input id
+
+  // for(let note of parsedNotes){
+  //   if(note.id === id){
+       parsedNotes.splice(noteIndex,1)
+      
+  //   }
+  // }
+  const noteString = JSON.stringify(parsedNotes);
+  fs.writeFile(`./db/db.json`, noteString, (err) =>
+  err
+    ? console.error(err)
+    : console.log(
+        `Note has been deleted from JSON file`
+      )
+);
+    }
+
+})
 })
 
 //Have the server run on port 3001
