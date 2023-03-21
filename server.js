5015eac2872b77af92a5dfd014eee6c4e1b72cd2
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-03-20 21:42:07 -0700

    finished server side

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 const readFromFile = util.promisify(fs.readFile);
 app.use(express.static('public'));
 // Shows the homepage anytime a url is entered aside from /notes
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname,
-      `/public/index.html`))
-  );
-  
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname,
+    `/public/index.html`))
+);
+
 
 //Pulls notes.html when /notes is used at the end of url
 
@@ -30,20 +30,20 @@ app.get('/notes', (req, res) =>
 
 //Gets all data stored in JSON file and shows it on the page
 
-app.get('/api/notes',(req, res) => {
+app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
 
 //Adds another item to JSON file
 //Need to read in the JSON file first so you can append new data and then resave the JSON
-app.post('/api/notes',(req, res) => {
+app.post('/api/notes', (req, res) => {
 
   //deconstruct post data
-  const {title, text} = req.body;
-  
+  const { title, text } = req.body;
+
   //If data is complete create a new note object
-  if(title && text){
+  if (title && text) {
     newNote = {
       title,
       text,
@@ -51,24 +51,24 @@ app.post('/api/notes',(req, res) => {
     }
   }
 
-  fs.readFile(`./db/db.json`,'utf8',(err,data)=>{
-    if(err){
+  fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+    if (err) {
       console.error(err)
     }
-    else{
-    const parsedNotes = JSON.parse(data);
-    parsedNotes.push(newNote);
-    const noteString = JSON.stringify(parsedNotes);
+    else {
+      const parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNote);
+      const noteString = JSON.stringify(parsedNotes);
 
-    fs.writeFile(`./db/db.json`, noteString, (err) =>
-    err
-      ? console.error(err)
-      : console.log(
-          `Review for ${newNote.title} has been written to JSON file`
-        )
-  );
-}
-})
+      fs.writeFile(`./db/db.json`, noteString, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+            `Review for ${newNote.title} has been written to JSON file`
+          )
+      );
+    }
+  })
 
   //Create section to read in current JSON
   //Append new Note to old JSON
@@ -76,38 +76,38 @@ app.post('/api/notes',(req, res) => {
 })
 
 //Deletes JSON data based on unique idea in url
-app.delete(`/api/notes/:id`,(req, res) => {
+app.delete(`/api/notes/:id`, (req, res) => {
   const id = req.params.id
   console.log(id)
-   fs.readFile(`./db/db.json`,'utf8',(err,data)=>{
-    if(err){
+  fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+    if (err) {
       console.error(err)
     }
-    else{
+    else {
 
-    const parsedNotes = JSON.parse(data);
-    console.log(parsedNotes)
-  
-   const noteIndex = parsedNotes.findIndex(n => n.id == id)
-  //Loop through parsed Notes to find the note with the same input id
+      const parsedNotes = JSON.parse(data);
+      console.log(parsedNotes)
 
-  // for(let note of parsedNotes){
-  //   if(note.id === id){
-       parsedNotes.splice(noteIndex,1)
-      
-  //   }
-  // }
-  const noteString = JSON.stringify(parsedNotes);
-  fs.writeFile(`./db/db.json`, noteString, (err) =>
-  err
-    ? console.error(err)
-    : console.log(
-        `Note has been deleted from JSON file`
-      )
-);
+      const noteIndex = parsedNotes.findIndex(n => n.id == id)
+      //Loop through parsed Notes to find the note with the same input id
+
+      // for(let note of parsedNotes){
+      //   if(note.id === id){
+      parsedNotes.splice(noteIndex, 1)
+
+      //   }
+      // }
+      const noteString = JSON.stringify(parsedNotes);
+      fs.writeFile(`./db/db.json`, noteString, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+            `Note has been deleted from JSON file`
+          )
+      );
     }
 
-})
+  })
 })
 
 //Have the server run on port 3001
